3de85b6a7a8b20dc2e399ee95644378534c6f59e
IanSani <iansani259@gmail.com>
2019-04-24 15:38:54 +0300

    Added more app details to the README file

--- app/main/forms.py
+++ app/main/forms.py
@@ -5,7 +5,7 @@ from wtforms.validators import Required
 
 class PitchForm(FlaskForm):
     title = StringField('Pitch Title')
-    category = SelectField(u'Pitch Category', choices=[('life', 'life'), ('coding', 'coding'), ('funny', 'funny')])
+    category = SelectField(u'Pitch Category', choices=[('Interviews', 'Interviews'), ('Lifestyle', 'Lifestyle'), ('funny', 'funny')])
     pitch = TextAreaField('Pitch')
     submit = SubmitField('Submit')
 
